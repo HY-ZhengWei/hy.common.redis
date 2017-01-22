8340f178ad91b648e17ebf409d3ac3a18496e13a
HY-ZhengWei <HY.ZhengWei@qq.com>
2017-01-22 18:08:35 +0800

    Change-Id: I97a788b6943ba9733b08b885f404076f06f2472f Signed-off-by: HY-ZhengWei <HY.ZhengWei@qq.com>

--- make_Common.bat
+++ make_Common.bat
@@ -2,6 +2,6 @@ cd bin
 
 rd /s /q .\com\hy\common\redis\junit
 
-jar cvfm hy.common.redis.jar MANIFEST.MF com
+jar cvfm hy.common.redis.jar MANIFEST.MF LICENSE com
 copy hy.common.redis.jar ..
 del /q hy.common.redis.jar
--- make_Common.sh
+++ make_Common.sh
@@ -5,7 +5,7 @@ cd ./bin
 
 rm -R ./org/hy/common/redis/junit
 
-jar cvfm hy.common.redis.jar MANIFEST.MF org
+jar cvfm hy.common.redis.jar MANIFEST.MF LICENSE org
 cp hy.common.redis.jar ..
 rm hy.common.redis.jar
 cd ..
