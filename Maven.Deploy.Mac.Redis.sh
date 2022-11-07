be2f5b3c871662effbf1f0b2af5bca33b82b227f
ZhengWei <HY.ZhengWei@qq.com>
2022-11-07 18:45:48 +0800

    1.0.2
    1.添加：测试代码

--- Maven.Deploy.Mac.Redis.sh
+++ Maven.Deploy.Mac.Redis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mvn deploy:deploy-file -Dfile=hy.common.redis.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/redis/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
-mvn deploy:deploy-file -Dfile=hy.common.redis-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/redis/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
+mvn deploy:deploy-file -Dfile=hy.common.redis.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/redis/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:1481/repository/thirdparty
+mvn deploy:deploy-file -Dfile=hy.common.redis-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/redis/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:1481/repository/thirdparty
